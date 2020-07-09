d0f20afafe1b90bf735964e2e05143b867b0ebe1
bjoberg <brett.oberg8@gmail.com>
2020-07-09 16:23:23 -0500

    Start designing shopping cart item

---
+++ src/components/ShoppingCartItem.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="shopping-cart-item-container">
+    <img class="shopping-cart-item-image" :alt="title" :src="imageSrc" />
+    <div>
+      <h1>{{title}}</h1>
+      <p>{{price}}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ShoppingCartItem",
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    price: {
+      type: String,
+      required: true
+    },
+    imageSrc: {
+      type: String,
+      required: true
+    }
+  }
+};
+</script>
+
+<style>
+.shopping-cart-item-container {
+  display: flex;
+  flex-direction: row;
+  margin: 40px;
+  border: 1px solid black;
+}
+img.shopping-cart-item-image {
+  border: 1px solid #d8d8d8;
+  max-width: 200px;
+  margin: 40px;
+  box-shadow: 0px 0.5px 1px #d8d8d8;
+}
+</style>
